677e2702cea8dac879494bb97ff83accb9c5404c
mpppk <niboshiporipori@gmail.com>
2017-05-01 17:30:54 +0900

    Fix browse command

--- cmd/browse.go
+++ cmd/browse.go
@@ -1,22 +1,27 @@
 package cmd
 
 import (
-	"github.com/spf13/cobra"
-	"github.com/mpppk/gico/gico"
 	"context"
-	"github.com/skratchdot/open-golang/open"
 	"os"
+
+	"github.com/mpppk/gico/gico"
+	"github.com/skratchdot/open-golang/open"
+	"github.com/spf13/cobra"
 )
 
 // browseCmd represents the browse command
 var browseCmd = &cobra.Command{
 	Use:   "browse",
 	Short: "browse repo",
-	Long: `browse repo`,
+	Long:  `browse repo`,
 	Run: func(cmd *cobra.Command, args []string) {
 		ctx := context.Background()
 		client := gico.GetGitHubClient(context.Background(), os.Getenv("GICO_GITHUB_TOKEN"))
-		issues, err := gico.GetIssues(ctx, client, "mpppk", "skylarq", nil)
+
+		originRemote, err := gico.GetOriginRemote()
+		gico.PanicIfErrorExist(err)
+
+		issues, err := gico.GetIssues(ctx, client, originRemote.Owner, originRemote.RepoName, nil)
 		gico.PanicIfErrorExist(err)
 
 		var issueTitles []string
--- gico/util.go
+++ gico/util.go
@@ -4,6 +4,8 @@ import (
 	"os/exec"
 	"io"
 	"strings"
+	"errors"
+	"regexp"
 )
 
 func PipeToPeco(texts []string) (string, error) {
@@ -23,5 +25,22 @@ func PipeToPeco(texts []string) (string, error) {
 func PanicIfErrorExist(err error) {
 	if err != nil {
 		panic(err)
+	}
+}
+
+func ParseRemoteURL(url string) (host, owner, repoName string, err error) {
+	if strings.Contains(url, "github.com") {
+		assined := regexp.MustCompile(`github\.com.(.*)`)
+		group := assined.FindStringSubmatch(url)
+
+		if len(group) < 2 {
+			return "", "", "", errors.New("invalid url: " + url)
+		}
+
+		ownerAndRepo := strings.Split(group[1], "/")
+		repoName := strings.Replace(ownerAndRepo[1], ".git", "", -1)
+		return "github", ownerAndRepo[0], repoName, nil
+	} else {
+		return "", "", "", errors.New("unknown host: " + url)
 	}
 }
