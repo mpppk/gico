e30d073d62bbd1c47d86b459fe90be8d8f11336d
mpppk <niboshiporipori@gmail.com>
2017-05-10 01:13:11 +0900

    Add config path for windows home

--- cmd/root.go
+++ cmd/root.go
@@ -50,6 +50,7 @@ func initConfig() {
 
 	viper.SetConfigName(".gico")           // name of config file (without extension)
 	viper.AddConfigPath(os.Getenv("HOME")) // adding home directory as first search path
+	viper.AddConfigPath(os.Getenv("USERPROFILE")) // adding home directory as second search path for windows
 	viper.AutomaticEnv()                   // read in environment variables that match
 
 	// If a config file is found, read it in.
